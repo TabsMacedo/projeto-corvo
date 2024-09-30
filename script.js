81a5ca341c733f84ff0cc672779e617575811d08
TabsMacedo <tabata.macedo1897@gmail.com>
2024-09-30 10:32:41 -0300

    finalização do site

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Cria uma instância do Compress
-    const compress = new Compress();
 
-    // Seleciona a imagem do header
-    const imgElement = new Image();
-    imgElement.src = '/assets/fundo.png';
-    imgElement.onload = function() {
-        compress.compress([imgElement], {
-            size: 4, // o tamanho máximo do arquivo de saída em MB
-            quality: 0.75, // qualidade da imagem comprimida
-            maxWidth: 1920, // largura máxima da imagem
-            maxHeight: 1080, // altura máxima da imagem
-            resize: true, // permite redimensionamento
-        }).then((data) => {
-            // 'data' é um array de imagens comprimidas
-            const compressedImage = data[0];
-            // Obtém o base64 da imagem comprimida
-            const compressedDataUrl = compressedImage.prefix + compressedImage.data;
-            // Atualiza o background image com a imagem comprimida
-            document.querySelector('.header').style.backgroundImage = `url(${compressedDataUrl})`;
+    document.addEventListener('DOMContentLoaded', function() {
+        const aboutSection = document.querySelector('.about-section');
+        const usLeft = document.querySelector('.us__left');
+        const usRight = document.querySelector('.us__right');
+
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    usLeft.classList.add('show');
+                    usRight.classList.add('show');
+                }
+            });
         });
-    };
-});
+
+        observer.observe(aboutSection);
+    });
+
